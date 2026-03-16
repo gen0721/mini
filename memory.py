e1b4ce64f2d20a58f2c4066b0aa76b44f6e973f1
gen0721 <gentre29@gmail.com>
2026-03-16 07:45:22 +0500

    Update memory.py

--- memory.py
+++ memory.py
@@ -1,27 +1,7 @@
-import asyncpg
-from config import DATABASE_URL
+from database import pool
 
-pool = None
-
-async def init_db():
-    global pool
-    pool = await asyncpg.create_pool(DATABASE_URL)
-
-    async with pool.acquire() as conn:
-        await conn.execute("""
-        CREATE TABLE IF NOT EXISTS notes(
-            id SERIAL PRIMARY KEY,
-            text TEXT,
-            created TIMESTAMP DEFAULT NOW()
-        );
-        """)
-
-async def save_note(text):
+async def save_fact(info):
     async with pool.acquire() as conn:
         await conn.execute(
-            "INSERT INTO notes(text) VALUES($1)", text
+            "INSERT INTO facts(info) VALUES($1)", info
         )
-
-async def get_notes():
-    async with pool.acquire() as conn:
-        return await conn.fetch("SELECT * FROM notes ORDER BY id DESC")
