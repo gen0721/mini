da3c843fa0114cf4944db20b7a0522baff11aec8
gen0721 <gentre29@gmail.com>
2026-03-16 07:46:59 +0500

    Update osint.py

--- osint.py
+++ osint.py
@@ -1,8 +1,8 @@
 import aiohttp
 
-async def search_info(query):
+async def osint_search(query):
     async with aiohttp.ClientSession() as s:
         url = f"https://api.duckduckgo.com/?q={query}&format=json"
         async with s.get(url) as r:
             data = await r.json()
-            return data.get("AbstractText", "Ничего не найдено")
+            return data.get("AbstractText", "Нет данных")
