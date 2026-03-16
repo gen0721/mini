3f07975395da390210bfbaa15be8565efda42b06
gen0721 <gentre29@gmail.com>
2026-03-16 07:38:13 +0500

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import asyncio
+
+from memory import init_db
+from userbot import start_userbot
+from control_bot import start_bot
+from agent import start_agent
+
+async def main():
+    await init_db()
+    await start_userbot()
+    await start_bot()
+    await start_agent()
+
+    while True:
+        await asyncio.sleep(3600)
+
+asyncio.run(main())
