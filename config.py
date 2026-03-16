ad488ceacfe193dd42d9c47189d528d6fa2913b1
gen0721 <gentre29@gmail.com>
2026-03-16 08:12:59 +0500

    Update config.py

--- config.py
+++ config.py
@@ -7,3 +7,6 @@ BOT_TOKEN = os.getenv("BOT_TOKEN")
 OWNER_ID = int(os.getenv("OWNER_ID"))
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+# Строковая сессия Telethon (генерируется один раз локально)
+SESSION_STRING = os.getenv("SESSION_STRING")
